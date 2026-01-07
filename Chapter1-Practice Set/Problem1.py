a5c85822acebd3d7726848adb419fc24dd3407aa
Debananda Kuanr <debanandakuanr453@gmail.com>
2026-01-07 16:30:20 +0530

    Add files via upload

---
+++ Chapter1-Practice Set/Problem1.py
@@ -0,0 +1,22 @@
+# Write a program to print Twinkle twinkle little star poem in python. 
+
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+      
+Twinkle Twinkle Little Star
+How I wonder what you are!
+      
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+      
+Twinkle Twinkle Little Star
+How I wonder what you are!''')
+
+
+# If we Want to Print multiline string in python we can use triple quotes(''' or """) inside the print() Function
+
+# Triple quotes allow the string to span multiple lines, preserving the line breaks and formatting as written in the code.
---
+++ Chapter1/first.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+
+# To See our output in The Terminal We have to run the following command:
+#     python first.py
